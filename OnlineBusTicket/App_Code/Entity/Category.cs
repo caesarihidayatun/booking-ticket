f57328b89860870787543ad2552f17fe7a9e38c2
dangngoctuyen <dangngoctuyen@gmail.com>
2011-02-08 15:35:58 +0000

    8/2/2011 - TuyenDN

---
+++ OnlineBusTicket/App_Code/Entity/Category.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+/// <summary>
+/// Summary description for Category
+/// </summary>
+public class Category
+{
+    private int categoryID;
+    private string categoryName;
+    private bool status;
+
+    public Category()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public int CategoryID
+    {
+        get { return categoryID; }
+        set { categoryID = value; }
+    }
+
+    public string CategoryName
+    {
+        get { return categoryName; }
+        set { categoryName = value; }
+    }
+
+    public bool Status
+    {
+        get { return status; }
+        set { status = value; }
+    }
+}
